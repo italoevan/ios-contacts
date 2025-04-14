8e1c93fd1c625d9c156d5ca1d7603a47731c1e76
italo <igbehh@gmail.com>
2025-04-14 16:13:09 -0300

    Initial Commit

---
+++ DesafioTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioTabBar
+//
+//  Created by Italo Gabriel Bezerra Evangelista on 14/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DesafioTabBarTests/DesafioTabBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioTabBarTests.swift
+//  DesafioTabBarTests
+//
+//  Created by Italo Gabriel Bezerra Evangelista on 14/04/25.
+//
+
+import Testing
+@testable import DesafioTabBar
+
+struct DesafioTabBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

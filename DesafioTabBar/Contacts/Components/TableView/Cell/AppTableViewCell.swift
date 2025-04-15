ad3cc8cc5aea43d26ec24c37ecd8e28036a02bff
italo <igbehh@gmail.com>
2025-04-14 21:53:38 -0300

    new changes

---
+++ DesafioTabBar/Contacts/Components/TableView/Cell/AppTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppTableViewCell.swift
+//  DesafioTabBar
+//
+//  Created by Italo Gabriel Bezerra Evangelista on 14/04/25.
+//
+
+import UIKit
+
+class AppTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DesafioTabBar/Contacts/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  DesafioTabBar
+//
+//  Created by Italo Gabriel Bezerra Evangelista on 14/04/25.
+//
+
+import Foundation
